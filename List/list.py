ddefec97ca668816e33906e8dc33c6be4b2cda9b
beka-tech <abeka039@gmail.com>
2022-12-23 19:39:08 +0300

    Python lesson

---
+++ List/list.py
@@ -0,0 +1,13 @@
+# list
+car = ['bmw', 'audi', 'toyot', 'mercedes']
+print(type(car))
+
+list1 = [0, 1, 2, 3, 4, 5, 6]
+print(type(list1))
+
+cararray = ["Ford", "Volvo", "BMW"]
+cararray.append("audio")
+print(cararray)
+
+fruit = ("apple", "orange", "banana")
+print(type(fruit))
