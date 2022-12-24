6158a5b99fbff4610853818d2f22e49400e06cf1
beka aman <63505760+beka-tech@users.noreply.github.com>
2022-12-24 23:01:37 +0300

    Update list.py

--- List/list.py
+++ List/list.py
@@ -6,7 +6,7 @@ list1 = [0, 1, 2, 3, 4, 5, 6]
 print(type(list1))
 
 cararray = ["Ford", "Volvo", "BMW"]
-cararray.append("audio")
+cararray.append("Audi")
 print(cararray)
 
 fruit = ("apple", "orange", "banana")
